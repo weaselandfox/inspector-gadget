9f8e22ccdd4282a96f5a899cb8b66e6bb7d935b2
yn5 <ynsnetmail@gmail.com>
2020-05-15 23:43:08 +0200

    Catch all routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
+app.post('*', (req, res) => {
   console.log('-----START REQUEST-----');
   console.log('HEADERS', JSON.stringify(req.headers));
   console.log('BODY', JSON.stringify(req.body));
